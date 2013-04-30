e1422a0369a57e4f0a98e2f524fb5b8806ae0995
Andy Leeb <ableeb@andersonleeb.com>
2013-04-30 14:42:17 -0400

    Prepare collective.portlets.lineage 1.2.6.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.2.6'
 setup(name='collective.portlets.lineage',
       version=version,
       description="A collection of collective.lineage aware portlets",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
